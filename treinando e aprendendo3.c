17e3b5cb474eaec08caf00fa88d662a2a3985044
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-09-04 21:54:53 -0300

    Create treinando e aprendendo3.c
    criando programa simples de soma de dois valores .

---
+++ treinando e aprendendo3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){         /*treinando e aprendendo
+                        soma de dois valores */
+        int n1,n2;
+        printf("digite dois valores:");
+        scanf("%d%d", &n1, &n2);
+        printf("o resultado de %d + %d =%d\n",n1,n2,n1+n2);
+
+
+
+
+}
